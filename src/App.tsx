cb69a54a25df0ff3e863155c8ec44c00985612ce
ek-mlg <ek.mlg@icloud.com>
2023-10-08 15:11:00 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'react'
+import { Button } from '@/components/ui/button'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>link button</h1>
+      <Button as={'a'} variant={'primary'} href={'/link'} />
+
+      <h1>classic button</h1>
+      <Button variant={'primary'} as={'button'} />
     </>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { StrictMode } from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { createRoot } from 'react-dom/client'
+
+import { App } from './App.tsx'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>
 )
